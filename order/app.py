0afcc839027dbd4042133c05a3b9034127be38ec
Matei Mirica <M.M.Mirica@student.tudelft.nl>
2025-03-28 17:26:33 +0100

    Implement database replication with replica failover

--- order/app.py
+++ order/app.py
@@ -11,6 +11,7 @@ from msgspec import msgpack, Struct
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import Response
 from faststream.kafka.fastapi import KafkaRouter
+from redis.sentinel import Sentinel
 
 from order_worker import OrderWorker, OrderValue
 
@@ -23,10 +24,23 @@ app = FastAPI(title="order-service")
 router = KafkaRouter("kafka:9092", logger=None)
 app.include_router(router)
 
-db: redis.Redis = redis.Redis(host=os.environ['REDIS_HOST'],
-                              port=int(os.environ['REDIS_PORT']),
-                              password=os.environ['REDIS_PASSWORD'],
-                              db=int(os.environ['REDIS_DB']))
+sentinel_hosts = [
+    (host.split(":")[0], int(host.split(":")[1]))
+    for host in os.environ["REDIS_SENTINEL_HOSTS"].split(",")
+]
+
+sentinel = Sentinel(
+    sentinel_hosts,
+    socket_timeout=0.5,
+    password=os.environ["REDIS_PASSWORD"]
+)
+
+db = sentinel.master_for(
+    os.environ["REDIS_MASTER_NAME"],
+    socket_timeout=0.5,
+    password=os.environ["REDIS_PASSWORD"],
+    db=int(os.environ.get("REDIS_DB", 0))
+)
 
 def close_db_connection():
     db.close()
